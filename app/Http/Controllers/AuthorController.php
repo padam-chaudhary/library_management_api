6783cc57709e3ade8d825723d3d91bd2ebb876f3
Padam chaudhary <chaudharypadam741@gmail.com>
2026-02-26 10:58:01 +0545

    feat(author): implement author endpoints and register API routes

---
+++ app/Http/Controllers/AuthorController.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Author;
+class AuthorController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $authors = Author::paginate(10);
+
+        return response()->json([
+            'authors' => $authors,
+            'message' => 'Authors retrieved successfully'
+        ], status: 200);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
